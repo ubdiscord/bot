ec3ae01996cc7d29be0ed520bcec6553992b5575
ubdiscord <46159760+ubdiscord@users.noreply.github.com>
2018-12-28 11:46:48 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import asyncio
+import colorsys
+import random
+import platform
+from discord import Game, Embed, Color, Status, ChannelType
+import os
+import functools
+import time
+import datetime
+import requests
+import json
+import praw
+import aiohttp
+
+prefix = os.getenv("PREFIX")
+
+Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
+client = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), pm_help = True)
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    print('--------')
+    print('Started Our BOT')
+    print('Created by Utkarsh')
+    
+client.run(os.getenv('Token'))
