2752513e118dc0962086f5336e71fbc2f191cf3a
ubdiscord <46159760+ubdiscord@users.noreply.github.com>
2018-12-31 13:35:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ prefix = os.getenv("PREFIX")
 desc= os.getenv("DESCRIPTION")
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = commands.Bot(description=desc,command_prefix=commands.when_mentioned_or(prefix), pm_help = True)
+client = commands.Bot(description=desc,command_prefix=prefix, pm_help = True)
 
 @client.event
 async def on_ready():
